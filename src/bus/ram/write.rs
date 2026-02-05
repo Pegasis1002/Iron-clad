6ab57b74a3d863d6c3a3395ede06fd948fce87f2
Pegasis1002 <thewebserfer1234@gmail.com>
2026-02-05 21:50:50 +0530

    restructed write funtion in bus

--- src/bus/ram/write.rs
+++ src/bus/ram/write.rs
@@ -2,17 +2,6 @@ use crate::models::bus::BUS;
 
 impl BUS {
     pub(crate) fn write(&mut self, addr: u32, data: u32) {
-        let bytes = data.to_le_bytes();
-
-        // VRAM check
-        if addr >= 0x1100_0000 && addr < 0x1104_B000 {
-            let index = ((addr - 0x1100_0000) / 4) as usize;
-            if index < self.vram.len() {
-                self.vram[index] = data;
-            }
-                println!("VRAM Write detected! Color: {:#010X}", data);
-            return;
-        }
 
         // UART check
         if addr == 0x1000_0000 {
@@ -22,11 +11,21 @@ impl BUS {
             return; // Don't try to write this to RAM!
         }
 
+        // VRAM
+        if addr >= 0x1100_0000 && addr < 0x1104_B000 {
+            let index = ((addr - 0x1100_0000) / 4) as usize;
+            if index < self.vram.len() {
+                self.vram[index] = data;
+            }
+            return;
+        }
+
         if addr < 0x8000_0000 {
             println!("Memory Access Error: Address {:#X} is below RAM start.", addr);
             return;
         }
 
+        let bytes = data.to_le_bytes();
         let index = (addr - 0x8000_0000) as usize;
         if index + 3 >= self.ram.len() {
             println!("Memory Access Error: Address {:#X} is out of RAM bounds.", addr);
