25dcfd750f9eeb4f349902aab1cbe6f0eeedd881
Pegasis1002 <thewebserfer1234@gmail.com>
2026-02-07 23:20:07 +0530

    completed the benchmark test with flying colors. The emulator is well optimized

--- src/cpu/exec/system.rs
+++ src/cpu/exec/system.rs
@@ -1,15 +1,16 @@
-use crate::{cpu::decode::DecodeInst, models::cpu::{CPU, Mode}};
+use crate::{cpu::{self, decode::DecodeInst}, models::cpu::{CPU, Mode}};
 
 pub(crate) fn exec_system(cpu: &mut CPU, inst: DecodeInst) {
     match inst.funct3 {
-        0b000 => {
+        0x0 => {
             match inst.imm {
                 0x000 => ecall(cpu),
                 0x302 => mret(cpu),
                 _ => panic!("Unkown system instruction")
             }
         },
-        0b001 => csrrw(cpu, inst),
+        0x1 => csrrw(cpu, inst),
+        0x2 => csrrs(cpu, inst),
         _ => {}
     }
 }
@@ -19,8 +20,12 @@ fn ecall(cpu: &mut CPU) {
 
     match syscall_no {
         93 => {
-            println!("INFO: Program exited via ECALL (93).");
-            std::process::exit(0);
+            cpu.csr[0x341] = cpu.pc;
+            cpu.csr[0x342] = 8;
+            
+            cpu.mode = Mode::Machine;
+            cpu.pc = cpu.csr[0x305].wrapping_sub(4);
+            println!("TRAP: ECALL caught. Jumping to Kernel at {:#X}", cpu.csr[0x305]);
         },
         _ => println!("Unhandled ECALL: syscall number {}", syscall_no)
     }
@@ -42,5 +47,18 @@ fn csrrw(cpu: &mut CPU, inst: DecodeInst){
 
     if inst.rd != 0 {
         cpu.reg[inst.rd as usize] = old_val
+    }
+}
+
+// This is the instruction that 'csrr' actually maps to
+fn csrrs(cpu: &mut CPU, inst: DecodeInst) {
+    let csr_addr = (inst.imm as usize) & 0xFFF;
+    let old_val = cpu.csr[csr_addr];
+
+    cpu.csr[csr_addr] = old_val | cpu.reg[inst.rs1 as usize];
+    
+    // We only care about reading for the benchmark
+    if inst.rd != 0 {
+        cpu.reg[inst.rd as usize] = old_val;
     }
 }
